89aae4f9a74d7832a2f35da4787bb6f8d0521cef
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-04-11 23:16:29 +0900

    set: 개발환경세팅 중

---
+++ Bypyeong-BackEnd/src/main/java/com/example/bypyeongbackend/BypyeongBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.bypyeongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BypyeongBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BypyeongBackEndApplication.class, args);
+    }
+
+}
---
+++ Bypyeong-BackEnd/src/test/java/com/example/bypyeongbackend/BypyeongBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bypyeongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BypyeongBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
